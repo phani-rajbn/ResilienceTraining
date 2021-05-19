450cd1f59ffbc51adf87af085fb8a4307c15eff8
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2021-05-19 10:26:34 +0530

    Add files via upload

---
+++ Day 6/DelegateExample.cs
@@ -0,0 +1,59 @@
+﻿using System;
+/*
+ * Delegates are similar to function pointers of C++. They are references to methods so that U can invoke them during runtime based on the function it is refering to. They are used to perform callback operations, async operations within an application.
+ * Delegates are helpfull in creating methods that will take method as an arg instead of an object as an argument.
+ * Delegates are reference types, so U can create instances of the delegates inr UR code. 
+ * The method that U R passing to the delegate instance should have the same nomenclature. Its return type and its parametes must be matching to the delegate signature. 
+ * 
+*/
+namespace SampleConApp.Day_6
+{
+    //Creating a Delegate
+    delegate double ArithematicOperation(double v1, double v2);
+    class DelegateExample
+    {
+        //to pass a method as an argument
+        static void DoMathOperation(ArithematicOperation operation)
+        {
+            double first = MyConsole.GetDouble("Enter First Value");
+            double second = MyConsole.GetDouble("Enter Second Value");
+            double res = operation(first, second);//Invoking the passed method... 
+            Console.WriteLine("The Result: " + res);
+        }
+
+        /*A function is required only when U want to invoke these statements multiple times within the application execution*/
+
+        static double myFunc(double v1, double v2) => v1 * v2 - v1 + v2; 
+
+        static void Main(string[] args)
+        {
+            /*****************Old Syntax******************************
+            ArithematicOperation instance = new ArithematicOperation(myFunc);
+            DoMathOperation(instance);
+            **********************Different kind**********************************
+            ArithematicOperation instance = myFunc;
+            DoMathOperation(instance);
+            **********************.NET 2.0*************************
+            DoMathOperation(myFunc);
+            ***************************Anonymous methods*********************
+            DoMathOperation(delegate(double v1, double v2)
+            {
+                return v1 + v2;
+            });
+
+            DoMathOperation(delegate (double v1, double v2)
+            {
+                return v1 - v2;
+            });
+            *********************Using Lambda Expression******************************/
+            DoMathOperation((v1, v2) => {
+                Console.WriteLine("I am sitting inside a non informative method");
+                return v1 - v2;
+            });
+
+            DoMathOperation((apple, mango) => apple - mango);
+            //ToDo: convert all UR methods to lambda Expressions and see how it works.....
+        }
+    }
+}
+/*Delegates will be revisited while we do multi threading. */
---
+++ Day 6/EventHandling.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Events are call back functions which are instances of the delegates. They are invoked like Triggers of SQL server. When an action is performed by the caller, the event is triggered. Click is an event, TextChange is an event, when the user of the app performs that action, UR method gets invoked. Else U dont invoke the method.
+//event is the keyword used to declare an event. 
+//.NET offers a ready to use delegates which can be used in ur application, however for our practice, we will create a new delegate which is  void delegate and takes string as arg...
+namespace SampleConApp.Day_6
+{
+    delegate void EventHandler(string message);
+
+    class CRUDOperation
+    {
+        //internal members are available to all the classes within the project...
+        internal event EventHandler callBack = null;
+
+        public CRUDOperation(EventHandler callBack)
+        {
+            this.callBack = callBack;
+        }
+        HashSet<string> values = new HashSet<string>();
+        internal void Add(string value)
+        {
+            if(values.Add(value))
+            {
+                if(callBack != null)
+                {
+                    callBack("Added Successfully");
+                }
+            }
+            else
+            {
+                callBack("Duplicate value");
+            }
+        }
+
+        internal void Delete(string value)
+        {
+            if (values.Remove(value))
+                callBack("Removed Successfully");
+            else
+                callBack("No object found to delete");
+        }
+
+    }
+    class EventHandling
+    {
+        static void Main(string[] args)
+        {
+            CRUDOperation myApp = new CRUDOperation((msg) => Console.WriteLine(msg));
+            myApp.Add("Apples");
+            myApp.Add("Apples");
+            myApp.Add("Apples");
+            myApp.Delete("Apples");
+
+        }
+
+       
+    }
+}
