e6a5c6f36703eea1c5eaa9ee9ce1b9be30402cfd
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2021-05-26 14:39:39 +0530

    Add files via upload

---
+++ Day 11/ConnectedModel.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+//Data Access Models for connecting to the database and perform Data related operations. 
+//ADO.NET, LINQ, ENTITY FRAMEWORK
+//ADO.NET was the first in .NET to work on Data Access. 
+//ADO.NET has created seperate namespaces for each kind of database. U can download the DLLs for specific Databases from their respective websites. They contain seperate classes for each kind of database for optimization purposes. Each class is designed to work exclusively for that DB only. 
+//ADO.NET has ready to use APIs for SQL server and U dont need to download any further DLLs. 
+//ADO.NET is designed to work on any kind of databases. 
+//System.Data.SqlClient namespace is used to use the Classes for SQL server. 
+//For connecting to the database: SqlConnection Class
+//For passing queries: SqlCommand Class. Queries can be executed using ExecuteReader(SELECT), ExecuteNonQuery(INSERT, DELETE, UPDATE) and ExecuteScalar(_________).
+//For Reading data from SELECT queries: SqlDataReader class.
+namespace DatabaseApps
+{
+    class Employee
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public string EmpAddress { get; set; }
+        public double EmpSalary { get; set; }
+        public int DeptId { get; set; }
+        public string PhoneNo { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format($"{EmpName} from {EmpAddress} earns a salary of {EmpSalary}");
+        }
+    }
+    class ConnectedModel
+    {
+        //ConnectionString is important as it contains the info required to connect to the database: ServerName, DBName, UserName, Password, CredentialType...
+        const string CONNECTIONSTRING = @"Data Source=AKSHAY-PC\SQLEXPRESS;Initial Catalog=CSharpProject;Integrated Security=True";
+        const string SELECT = "SELECT * FROM TBLEMPLOYEE";
+        const string UPDATESTATEMENT = "UpdateEmployee";//set the name of the Stored proc...
+
+        internal static void ConnectToDB()
+        {
+            SqlConnection con = new SqlConnection(CONNECTIONSTRING);
+            SqlCommand cmd = new SqlCommand(SELECT, con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+                Console.WriteLine(reader[1]);
+            con.Close();
+        }
+        
+        private static Employee createEmployee(SqlDataReader reader)
+        {
+            Employee emp = new Employee//populate columns into respective Employee's properties.
+            {
+                EmpID = Convert.ToInt32(reader[0]),
+                EmpName = reader[1].ToString(),
+                EmpAddress = reader[2].ToString(),
+                PhoneNo = reader[3].ToString(),
+                EmpSalary = Convert.ToDouble(reader[4]),
+                DeptId = int.Parse(reader[5].ToString())
+            };
+            return emp;
+        }
+        internal static List<Employee> GetAllEmployees()
+        {
+            List<Employee> all = new List<Employee>();//create a empty list of Employees
+            SqlConnection con = new SqlConnection(CONNECTIONSTRING);//create the connection
+            SqlCommand cmd = new SqlCommand(SELECT, con);//create the command with requried statement
+            try
+            {
+                con.Open();//Open the connection
+                var reader = cmd.ExecuteReader();//execute the Command to return a reader object. 
+                if (!reader.HasRows)//Check for empty rows....
+                    throw new Exception("No Data populated to the Table");
+                while (reader.Read())//read each row in a while loop
+                {
+                    var emp = createEmployee(reader);
+                    all.Add(emp);//Add the object to the list
+                }
+                return all;//return the list
+            }
+            catch (SqlException)//handle any exceptions
+            {
+                throw new Exception("Reading has failed");
+            }
+            finally//last block to execute
+            {
+                con.Close();//Close the connection.
+            }
+        }
+
+        internal static List<Employee> FindEmployee(string name)
+        {
+            string strQUERY = $"select * from tblEmployee where EmpName like '%{name}%'";
+            SqlConnection con = new SqlConnection(CONNECTIONSTRING);
+            SqlCommand cmd = new SqlCommand(strQUERY, con);
+            List<Employee> list = new List<Employee>();
+            try
+            {
+                con.Open();
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    list.Add(createEmployee(reader));
+                }
+                return list;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private static void Execute(String statement)
+        {
+            //ExecuteNonQuery is used when UR command statement does not have SELECT in it...
+            SqlConnection con = new SqlConnection(CONNECTIONSTRING);
+            SqlCommand cmd = new SqlCommand(statement, con);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException)
+            {
+                throw new Exception("Insertion failed");
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        internal static void InsertEmployee(Employee emp)
+        {
+            string query = $"Insert into tblEmployee values({emp.EmpID}, '{emp.EmpName}','{emp.EmpAddress}','{emp.PhoneNo}', {emp.EmpSalary} , {emp.DeptId})";
+            Execute(query);
+            Console.WriteLine("Employee Inserted");
+
+        }
+
+        internal static void UpdateEmployee(Employee emp)
+        {
+            SqlConnection con = new SqlConnection(CONNECTIONSTRING);
+            SqlCommand cmd = new SqlCommand(UPDATESTATEMENT, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@id", emp.EmpID);
+            cmd.Parameters.AddWithValue("@name", emp.EmpName);
+            cmd.Parameters.AddWithValue("@address", emp.EmpAddress);
+            cmd.Parameters.AddWithValue("@phone", emp.PhoneNo);
+            cmd.Parameters.AddWithValue("@sal", emp.EmpSalary);
+            cmd.Parameters.AddWithValue("@dept", emp.DeptId);
+
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
+//ToDo:
+/*
+ * Create a Table called Review: ReviewID, MovieName, ReviewDetails, Rating.
+ * Create an interface called IMovieReview with all CRU functions
+ * IMplement the interface with Connected model code
+ * Call the interface object in the Console App with Menu driven Application.
+ */
---
+++ Day 11/NewFeatures.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+//Majority of these features first came up in .NET 3.5(2008) and was fully stablized in .NET 4.0(2010). 
+namespace DatabaseApps
+{
+    static class MyExtensions
+    {
+        /*
+         * Extension methods are added to the instance of the class, not to the class itself. 
+         * Extension methods are always static and its first parameter will always be a reference of the class on which U wish to extend this method. 
+         * U cannot have extension properties or fields. 
+         * Extension methods are not the part of method overriding, so UR Runtime polymorphism wont work on extension methods.
+         * Use extension methods for adding one or more functionality for existing class that cannot be extended. 
+         * .NET when it introduced LINQ and new frameworks, it did not want to break the existing interfaces, so it added the new methods as extension methods to them. 
+         */
+        public static int GetWords(this String content)
+        {
+            var words = content.Split(',', ' ', '}', '{');
+            return words.Length;
+        }
+    }
+    class NewFeatures
+    {
+        internal static void UsingVarKeyword()
+        {
+            //In C# 3.0, var keyword was introduced. It is a convinient way of declaring local variables within ur code. It is also called as implicit typed variables. The var variable when declared will be assigned to any data, the data type of that data will be implicitly infered to the variable.
+            //U must assign to the var variable while declaring itself.
+            //var cannot be used as parameter or return type of a function
+            //var is used only for local variables. 
+            //var cannot be used as fields of a class. 
+            //var automatically sets itself to the data type it is assigned to without knowing the actual type. Very helpful for storing return values of functions.
+            //There is no need for boxing and unboxing in var like the way U do in objects.
+            var intValue = 123;//integer.
+            var stringValue = "Apple, Mango, Orange"; //convinience of declaring local variable
+            var someValue = DateTime.Now ;
+            Console.WriteLine(++intValue);
+            Console.WriteLine(stringValue.ToUpper());
+            Console.WriteLine(someValue.ToString("dd-MMM-yyyy"));
+
+            var intArray = new int[] { 234, 45, 5, 454, 34 };
+            foreach (var item in intArray) Console.WriteLine(item);
+            Console.WriteLine("The size: " + intArray.Length);
+        }
+
+        internal static void ExtensionMethods()
+        {
+            String contents = File.ReadAllText("../../NewFeatures.cs");
+            Console.WriteLine(contents.GetWords());
+            
+        }
+
+        internal static void LambdaExpression()
+        {
+            //Lambda Expressions help in creating methods in a quick manner so that it can be used within a function or can be passed as an argument to a function. 
+            List<String> fruits = new List<string>();
+            fruits.Add("Apples");
+            fruits.Add("Mangoes");
+            fruits.Add("Oranges");
+            fruits.Add("Bananas");
+            fruits.Add("PineApples");
+            fruits.Add("Grapes(White)");
+            fruits.Add("Grapes(Purple)");
+            fruits.Add("Lime");
+            //List has funtions called Find, FindAll which takes a funtion as arg that defines the criteria of finding.
+            //Find takes an instance of a delegate called Predicate which points to a function that defines the criteria of finding. This funtion will be called internally on every object of the List, whichever returns true will be the found object. 
+
+            Console.WriteLine("Enter the Fruit to search");
+            string name = Console.ReadLine();
+            
+            var foundFruit = fruits.Find((fruit) => fruit == name);
+            Console.WriteLine(foundFruit);
+            Console.WriteLine("Now trying to find all fruits matching a condition");
+            Console.WriteLine("Enter the Fruit name or part of the name to search");
+            name = Console.ReadLine();
+            var foundFruits = fruits.FindAll((f) => f.Contains(name));
+            foreach (var fruit in foundFruits) Console.WriteLine( fruit);
+        }
+
+        internal static void UsingAnonymousTypes()
+        {
+            //U have created an object of an unknown or anonymous type. There is no class created, but U have created an object using new operator. These objects are used to create on the fly objects for displaying purpose or reading purpose. Usually the data will be filled from a result of a Query from the Database or from a file.  
+            //It will not have the features of OOP. No methods are creatable in these objects. They are simply data carriers. 
+            var empDetails = new
+            {
+                EmpID = 123,
+                EmpName ="Phaniraj",
+                EmpAddress ="Bangalore"
+            };
+            Console.WriteLine(empDetails);
+            Console.WriteLine($"The Name: {empDetails.EmpName}\nThe Address: {empDetails.EmpAddress}\nThe ID: {empDetails.EmpID}");
+            Console.WriteLine("The data type: " + empDetails.GetType().Name);
+        }
+    }
+}
