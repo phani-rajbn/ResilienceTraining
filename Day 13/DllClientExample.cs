60e722e485c9f72fd271b43a7c439f6d4842bf9e
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2021-05-28 10:48:40 +0530

    Add files via upload

---
+++ Day 13/DllClientExample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SampleDll;
+using System.Data;
+
+namespace SampleConApp.Day_13
+{
+    class UsingDllExample
+    {
+        static void Main(string[] args)
+        {
+            var component = ComponentFactory.GetComponent();
+            var table = component.GetAllEmployees();
+            foreach(DataRow row in table.Rows)
+            {
+                Console.WriteLine(row[1]);//2nd column data
+            }
+        }
+    }
+}
