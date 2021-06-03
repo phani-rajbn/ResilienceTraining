944e5a3be37f5309c05d0de3d83c3179a149c2f0
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2021-06-03 10:27:40 +0530

    Add files via upload

---
+++ JavaScriptApp/Scripts/Example1.js
@@ -0,0 +1,20 @@
+﻿function testFunc(v1, v2, v3) {
+    if (v1 != undefined)
+        console.log(v1);
+    if (v2 != undefined)
+        console.log(v2);
+    if (v3 != undefined)
+        console.log(v3);
+}
+
+//JS funtions will automatically have optional arguements, if the caller dont pass the arg, the function must have a logic to evade it. 
+//testFunc();
+
+function addFunc(first, second) {
+    return first + second;
+}
+
+function mulFunc(first, second) {
+    return first * second;
+}
+
